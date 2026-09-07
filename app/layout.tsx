43ea7dd6c45fa7092bc6c9eac26752304680dabc
nichoblackofc1-coder <nichoblackofc1-coder@users.noreply.github.com>
2026-09-07 01:04:03 -0300

    chore: remover pixels antigos e configurar Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -61,22 +61,36 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://hebbkx1anhila5yf.public.blob.vercel-storage.com" crossOrigin="anonymous" />
         <link rel="dns-prefetch" href="https://hebbkx1anhila5yf.public.blob.vercel-storage.com" />
-        <link rel="preconnect" href="https://cdn.utmify.com.br" crossOrigin="anonymous" />
-        <link rel="dns-prefetch" href="https://cdn.utmify.com.br" />
+        <link rel="preconnect" href="https://connect.facebook.net" crossOrigin="anonymous" />
+        <link rel="dns-prefetch" href="https://connect.facebook.net" />
         <link rel="preload" as="image" href="/images/colar-nossa-senhora.webp" type="image/webp" fetchPriority="high" />
         <link rel="preload" as="image" href="/images/colar-compre1-leve2.webp" type="image/webp" />
-        <Script id="utmify-pixel" strategy="lazyOnload">
+        <Script id="meta-pixel" strategy="afterInteractive">
           {`
-            window.pixelId = "69eff0f80c472724bb1044bd";
-            var a = document.createElement("script");
-            a.setAttribute("async", "");
-            a.setAttribute("defer", "");
-            a.setAttribute("src", "https://cdn.utmify.com.br/scripts/pixel/pixel.js");
-            document.head.appendChild(a);
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1380214147572496');
+            fbq('track', 'PageView');
           `}
         </Script>
       </head>
       <body className={`${inter.variable} ${poppins.variable} font-sans antialiased`}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1380214147572496&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
         {process.env.NODE_ENV === 'production' && <Analytics />}
       </body>
